026b07c58eb06c0b5fa9a8f8451a49d9edfe9a10
3DHACKPLANT <147284474+kimminhyeok11@users.noreply.github.com>
2025-11-06 17:00:52 +0900

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    "./index.html",
+    "./js/**/*.js"
+  ],
   theme: {
     extend: {},
   },
