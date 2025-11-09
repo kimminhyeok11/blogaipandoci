4b0dfa5f95f03bbe82b6d2be64d99dcbc94cd054
3DHACKPLANT <147284474+kimminhyeok11@users.noreply.github.com>
2025-11-09 16:05:19 +0900

    Delete scripts directory

--- scripts/bundle-entry.js
+++
@@ -1,9 +0,0 @@
-// Esbuild bundle entry: include core scripts in order
-import '../js/config.js';
-import '../js/dom-utils.js';
-import '../js/ui-components.js';
-import '../js/blog-app.js';
-// defer modules also bundled to avoid extra network trips
-import '../js/layout-manager.js';
-import '../js/script-loader.js';
-import '../js/perf-monitor.js';
